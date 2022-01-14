ac5f6d5571844c1161065512952e1b84c101044a
Eugene <18848414101@163.com>
2022-01-14 10:36:11 +0800

    new

--- 09_Example/01_Example/demo.cpp
+++ 09_Example/01_Example/demo.cpp
@@ -15,47 +15,7 @@ void Demo::setProperties() {
 }
 
 void Demo::setLayout() {
-    this->toolBar->addAction(this->actionInitList);
-    this->toolBar->addAction(this->actionClear);
-    this->toolBar->addAction(this->actionInput);
-    this->toolBar->addAction(this->actionAddItem);
-    this->toolBar->addAction(this->actionDeleteItem);
-    this->toolBar->addSeparator();
 
-    this->addToolBar(Qt::TopToolBarArea, this->toolBar);
-
-    this->toolBox->addItem(this->page1, QString("QListWidget"));
-    this->toolBox->addItem(this->page2, QString("QTreeWidget"));
-    this->toolBox->addItem(this->page3, QString("QTableWidget"));
-
-    this->tabWidget->addTab(this->tab1, QString("QListWidget"));
-    this->tabWidget->addTab(this->tab2, QString("QTreeWidget"));
-    this->tabWidget->addTab(this->tab3, QString("QTableWidget"));
-
-    this->vLayout1Page1->addWidget(this->toolButtonInit);
-    this->vLayout1Page1->addWidget(this->toolButtonClear);
-    this->vLayout1Page1->addWidget(this->toolButtonInput);
-    this->vLayout1Page1->addWidget(this->toolButtonAdd);
-    this->vLayout1Page1->addWidget(this->toolButtonDelete);
-
-    this->splitter->addWidget(this->toolBox);
-    this->splitter->addWidget(this->tabWidget);
-
-    this->hLayoutBoxTab1->addWidget(this->label);
-    this->hLayoutBoxTab1->addWidget(this->lineEdit);
-    this->hLayoutBoxTab1->addWidget(this->checkBox);
-    this->vLayout2Page1->addWidget(this->gBox1Tab1);
-    this->hLayoutBoxTab2->addWidget(this->toolButtonSelectItem);
-    this->hLayoutBoxTab2->addWidget(this->toolButtonSelectAll);
-    this->hLayoutBoxTab2->addWidget(this->toolButtonSelectNone);
-    this->hLayoutBoxTab2->addWidget(this->toolButtonSelectInv);
-    this->vLayout2Page1->addWidget(this->gBox2Tab1);
-    this->vLayout2Page1->addWidget(this->listWidget);
-
-    this->hLayoutMain->addWidget(this->splitter);
-    this->verticalLayout->addLayout(this->hLayoutMain);
-
-    this->setCentralWidget(this->centralWid);
 }
 
 void Demo::signalsAndSlots() {
