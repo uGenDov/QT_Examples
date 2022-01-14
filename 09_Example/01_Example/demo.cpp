5153332fcd45d91c323d2fab4466c048e5ba4ec8
Eugene <18848414101@163.com>
2022-01-14 10:35:28 +0800

    new

--- 09_Example/01_Example/demo.cpp
+++ 09_Example/01_Example/demo.cpp
@@ -2,82 +2,16 @@
 
 Demo::Demo(QMainWindow *parent) : QMainWindow(parent) {
     {
-        this->centralWid          = new QWidget(this);
-        this->hLayoutMain         = new QHBoxLayout();
-        this->toolBar             = new QToolBar(this);
 
-        this->verticalLayout      = new QVBoxLayout(this->centralWid);
-
-        this->actionInitList      = new QAction(QIcon("D:/images/bold.png"), QString("Initialize List"), this);
-        this->actionClear         = new QAction(QIcon("D:/images/bold.png"), QString("Clear List"), this);
-        this->actionInput         = new QAction(QIcon("D:/images/bold.png"), QString("Input"), this);
-        this->actionAddItem       = new QAction(QIcon("D:/images/bold.png"), QString("Add item"), this);
-        this->actionDeleteItem    = new QAction(QIcon("D:/images/bold.png"), QString("Delete item"), this);
-
-        this->splitter            = new QSplitter(this->centralWid);
-        this->toolBox             = new QToolBox(this->splitter);
-        this->tabWidget           = new QTabWidget(this->splitter);
-
-        this->page1               = new QWidget();
-        this->page2               = new QWidget();
-        this->page3               = new QWidget();
-        this->tab1                = new QWidget();
-        this->tab2                = new QWidget();
-        this->tab3                = new QWidget();
-
-        this->vLayout1Page1       = new QVBoxLayout(this->page1);
-
-        this->toolButtonInit      = new QToolButton(this->page1);
-        this->toolButtonClear     = new QToolButton(this->page1);
-        this->toolButtonInput     = new QToolButton(this->page1);
-        this->toolButtonAdd       = new QToolButton(this->page1);
-        this->toolButtonDelete    = new QToolButton(this->page1);
-
-       this->vLayout2Page1        = new QVBoxLayout(this->tab1);
-
-       this->gBox1Tab1            = new QGroupBox(this->tab1);
-       this->hLayoutBoxTab1       = new QHBoxLayout(this->gBox1Tab1);
-       this->label                = new QLabel(QString("Current item"), this->gBox1Tab1);
-       this->lineEdit             = new QLineEdit(this->gBox1Tab1);
-       this->checkBox             = new QCheckBox(this->gBox1Tab1);
-       this->gBox2Tab1            = new QGroupBox(this->tab1);
-       this->hLayoutBoxTab2       = new QHBoxLayout(this->gBox2Tab1);
-       this->toolButtonSelectItem = new QToolButton(this->gBox2Tab1);
-       this->toolButtonSelectAll  = new QToolButton(this->gBox2Tab1);
-       this->toolButtonSelectNone = new QToolButton(this->gBox2Tab1);
-       this->toolButtonSelectInv  = new QToolButton(this->gBox2Tab1);
-       this->listWidget           = new QListWidget(this->tab1);
     }
 
     {
-        Demo::setProperties();
-        Demo::setLayout();
         Demo::setFont(QFont("Arial", 9, -1, false));
-//        Demo::setFixedSize(QWidget::sizeHint().width(), QWidget::sizeHint().height());
     }
 }
 
 void Demo::setProperties() {
-    this->toolBar->setMovable(false);
-    this->toolBar->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
 
-    this->toolBox->setMaximumWidth(200);
-    this->toolBox->setMinimumWidth(200);
-
-    this->splitter->setOrientation(Qt::Horizontal);
-
-    this->toolButtonInit->setText(QString("ToolButtonInit"));
-    this->toolButtonClear->setText(QString("ToolButtonClear"));
-    this->toolButtonInput->setText(QString("ToolButtonInput"));
-    this->toolButtonAdd->setText(QString("ToolButtonAdd"));
-    this->toolButtonDelete->setText(QString("ToolButtonDelete"));
-
-    this->toolButtonSelectItem->setPopupMode(QToolButton::MenuButtonPopup);
-
-    this->toolButtonSelectItem->setText(QString("ToolButton Select Item"));
-    this->toolButtonSelectAll->setText(QString("ToolButton Select All"));
-    this->toolButtonSelectNone->setText(QString("ToolButton Select None"));
-    this->toolButtonSelectInv->setText(QString("TooButton Select Inv"));
 }
 
 void Demo::setLayout() {
--- 09_Example/01_Example/demo.h
+++ 09_Example/01_Example/demo.h
@@ -1,15 +1,4 @@
 #include <QMainWindow>
-#include <QAction>
-#include <QToolBar>
-#include <QVBoxLayout>
-#include <QSplitter>
-#include <QToolBox>
-#include <QToolButton>
-#include <QGroupBox>
-#include <QLineEdit>
-#include <QLabel>
-#include <QCheckBox>
-#include <QListWidget>
 
 class Demo : public QMainWindow {
 public:
@@ -25,41 +14,5 @@ public slots:
     void signalsAndSlots();
 
 private:
-    QAction     *actionInitList;
-    QAction     *actionClear;
-    QAction     *actionInput;
-    QAction     *actionAddItem;
-    QAction     *actionDeleteItem;
-    QWidget     *centralWid;
-    QVBoxLayout *verticalLayout;
-    QHBoxLayout *hLayoutMain;
-    QSplitter   *splitter;
-    QToolBox    *toolBox;
-    QWidget     *page1;
-    QVBoxLayout *vLayout1Page1;
-    QToolButton *toolButtonInit;
-    QToolButton *toolButtonClear;
-    QToolButton *toolButtonInput;
-    QToolButton *toolButtonAdd;
-    QToolButton *toolButtonDelete;
-    QWidget     *page2;
-    QWidget     *page3;
-    QTabWidget  *tabWidget;
-    QWidget     *tab1;
-    QVBoxLayout *vLayout2Page1;
-    QGroupBox   *gBox1Tab1;
-    QHBoxLayout *hLayoutBoxTab1;
-    QLabel      *label;
-    QLineEdit   *lineEdit;
-    QCheckBox   *checkBox;
-    QGroupBox   *gBox2Tab1;
-    QHBoxLayout *hLayoutBoxTab2;
-    QToolButton *toolButtonSelectItem;
-    QToolButton *toolButtonSelectAll;
-    QToolButton *toolButtonSelectNone;
-    QToolButton *toolButtonSelectInv;
-    QListWidget *listWidget;
-    QWidget     *tab2;
-    QWidget     *tab3;
-    QToolBar    *toolBar;
+
 };
